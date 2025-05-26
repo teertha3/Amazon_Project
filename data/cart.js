a4df87f4e34824ca76f61973dac8da43a145cdeb
teertha3 <teerthainavolu@gmail.com>
2025-05-26 22:56:40 +0530

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart=JSON.parse(localStorage.getItem("cart"));
+export let cart;
+loadFromStorage();
+
+export function loadFromStorage(){
+    cart=JSON.parse(localStorage.getItem('cart'));
 
 if(!cart){
     cart= [
@@ -14,8 +18,7 @@ if(!cart){
     }
     ];
 }
-
-
+}
 
 function saveToStorage(){
     localStorage.setItem('cart',JSON.stringify(cart));
--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 describe('test suite:  formatCurrency',()=>{
     it('converts cents into dollars',()=>{
         expect(formatCurrency(2095)).toEqual('20.95');
@@ -7,4 +7,7 @@ describe('test suite:  formatCurrency',()=>{
     it('works with 0',()=>{
         expect(formatCurrency(0)).toEqual('0.00');
     })
+    it('rounds up to nearest cent',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
 });
